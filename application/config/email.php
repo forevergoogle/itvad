5c6f8b5ac616f790de21170629f0e6c410839123
Mith <truonghoangmi@gmail.com>
2016-09-15 10:30:01 +0700

    Add files via upload

---
+++ application/config/email.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['protocol']     = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.gmail.com';
+$config['smtp_port']    = '465';
+$config['smtp_user']    = 'tinhdev@gmail.com';
+$config['smtp_pass']    = 'na0123456';
+$config['mailtype']     = 'html';
+$config['charset']      = 'utf-8';
+$config['newline']      = "\r\n";
